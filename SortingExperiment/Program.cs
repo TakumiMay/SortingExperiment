96ebfdc0ffbf4d5c20eb3571a687e2691fbf6d47
TakumiMay <maythermyself@gmail.com>
2019-02-07 23:42:26 -0500

    first ignore

---
+++ SortingExperiment/Program.cs
@@ -0,0 +1,72 @@
+﻿using SortingExperiment;
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] arr = new int[] { 2, 5, -4, 11, 0, 18, 22, 67, 51, 6 };
+            int[] arr = createArr(10, false, true, 10, 29);
+
+            Console.WriteLine("Original array : ");
+            foreach (var item in arr)
+            {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine();
+
+            // QuickSort.sort(arr, 0, arr.Length - 1);
+            HeapSort.sort(arr);
+
+            Console.WriteLine();
+            Console.WriteLine("Sorted array : ");
+
+            foreach (var item in arr)
+            {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine();
+
+
+        }
+
+        static int[] createArr(int size, bool ascOrder, bool random, int min, int max)
+        {
+            int[] arr = new int[size];
+            if (random)
+            {
+                Random randomN = new Random();
+                for (int i = 0; i < size; i++)
+                {
+                    int randomNumber = randomN.Next(min, max);
+                    arr[i] = randomNumber;
+                }
+            }
+            else
+            {
+                Random randomN = new Random();
+                int randomNumber = randomN.Next(min, max);
+                arr[0] = randomNumber;
+                if (ascOrder)
+                {
+                    for (int i = 1; i < size; i++)
+                    {
+                        randomNumber++;
+                        arr[i] = randomNumber;
+                    }
+                }
+                else
+                {
+                    for (int i = 1; i < size; i++)
+                    {
+                        randomNumber--;
+                        arr[i] = randomNumber;
+                    }
+                }
+            }
+            return arr;
+        }
+    }
+}
