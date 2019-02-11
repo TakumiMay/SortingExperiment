bbc81d3309ca5ecdb77d2a4d5507893c354ceb1e
Felipe Castillo <jfcastillo1204@gmail.com>
2019-02-10 21:10:27 -0500

    UnitTest for HeapSort finished...almost

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -8,29 +8,29 @@ namespace UnitTests
     public class UnitTest1
     {
 
-
         [TestMethod]
-        public void sortSimple()
+        public void HeapSortSimple()
         {
             //Arrange
             int[] array = { 4, 16, 2, 5, 10, 60, 100, 102, 40, 20 };
 
             //Act
             HeapSort.sort(array);
-            
+
             int[] esperado = { 2, 4, 5, 10, 16, 20, 40, 60, 100, 102 };
 
             //Assert
-            for(int i = 0; i < esperado.Length; i++) {
+            for (int i = 0; i < esperado.Length; i++)
+            {
                 Assert.IsTrue(esperado[i] == array[i]);
             }
             //Assert.AreEqual(esperado, array); 
-            
+
 
         }
 
         [TestMethod]
-        public void SortSimple2()
+        public void HeapSortSimple2()
         {
             HeapSort hs = new HeapSort();
 
@@ -46,11 +46,51 @@ namespace UnitTests
         }
 
         [TestMethod]
-        public void prueba()
+        public void HeapSortSimple3()
         {
-            Assert.IsTrue(true);
+            int[] array = { 70, 45, 41, 39, 36, 30, 14, 5, 1, 0 };
+            HeapSort.sort(array);
+            int[] esperado = { 0, 1, 5, 14, 30, 36, 39, 41, 45, 70 };
+            for (int i = 0; i < esperado.Length; i++)
+            {
+                Assert.IsTrue(esperado[i] == array[i]);
+            }
         }
 
+        [TestMethod]
+        public void HeapSortBig()
+        {
+            int[] array = generateRandom(1000);
+            HeapSort.sort(array);
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                Assert.IsTrue(array[i] <= array[i+1]);
+            }
+        }
+        [TestMethod]
+        public void HeapSortBigger()
+        {
+            int[] array = generateRandom(1000000);
+            HeapSort.sort(array);
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                Assert.IsTrue(array[i] <= array[i + 1]);
+            }
+        }
+        public int[] generateRandom(int max)
+        {
+            int[] array = new int[max];
+            for (int i = 0; i < max; i++)
+            {
+                array[i] = (new Random()).Next(1, max);
+            }
 
+
+            return array;
+
+
+
+
+        }
     }
 }
